5237084cf95bd606256a031ecc78558a8b00cfca
emy-sg <imane1sg@gmail.com>
2021-11-19 00:03:08 +0100

    first commit

---
+++ Day_00/ex00/megaphone.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int	main(int argc, char **argv)
+{
+	if (argc > 1)
+	{
+		std::cout << "new" << std::endl;
+	}
+	else
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+}
