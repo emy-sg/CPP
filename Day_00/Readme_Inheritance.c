cc881369c248707b86df77a3f0172f32bebc6218
Imane Sghiouar <isghioua@e1r13p10.1337.ma>
2021-11-22 03:20:11 +0100

    second push

---
+++ Day_00/Readme_Inheritance.c
@@ -0,0 +1,8 @@
+							Inheritance in C++
+https://www.geeksforgeeks.org/inheritance-in-c/
+			-----------------------------------------
+
+ - Inheritance: is the capability of a class to "derive properties and characteristics from another class" is called "Inheritance".
+"Inheritance" is one of the most important feature of Object Oriented Programming.
+ "Sub Class": is the class that inherits properties from another class.
+ "Super Class": is the class whose properties are inherited by "Sub Class" is called  is 
