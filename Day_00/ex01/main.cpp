034038e5fd459ab4312c3beb601db8a07afdf43a
Imane Sghiouar <isghioua@e1r9p11.1337.ma>
2021-11-24 18:59:10 +0100

    five commit

--- Day_00/ex01/main.cpp
+++ Day_00/ex01/main.cpp
@@ -4,7 +4,28 @@
 // ==> If a ninth contact is added, replace the oldest contact.
 // ADD command, SEARCH command, EXIT command.
 
+class contact
+{
+	public:
+		std::string	first_name;
+		std::string last_name;
+		std::string nickname;
+		std::string phone_number;
+		std::string darkest_secret;
+};
+
 int main()
 {
 
+	while (1)
+	{
+		contact	new_contact;
+		std::cin << "The first name: " << new_contact.firstname << std::endl;
+		std::cin << "The last name: " << new_contact.last << std::endl;
+		std::cin << "The nickname: " << new_contact.nickname << std::endl;
+		std::cin << "The phone number: " << new_contact.phone_number << std::endl;
+		std::cin << "The darkest secret: " << new_contact.dark  est_secret << std::endl;
+	}
+	return (0);
 }
+
