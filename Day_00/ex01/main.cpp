e3463c7c55890b893a83027e2fd75320eaa01f34
Imane Sghiouar <isghioua@e1r13p9.1337.ma>
2021-11-24 01:25:08 +0100

    fourth commit

---
+++ Day_00/ex01/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+// No dynamic allocation && can't store more than 8 contacts.
+// ==> If a ninth contact is added, replace the oldest contact.
+// ADD command, SEARCH command, EXIT command.
+
+int main()
+{
+
+}
